e3e9a9ae85ded4891e5ab7b5e480d145f12268b0
lahuuman <akinbaber@gmail.com>
2024-11-05 16:22:42 +0700

    add nv gui

---
+++ ManagementPhoneStore/NhanVienChiTietGUI.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace ManagementPhoneStore
+{
+    partial class NhanVienChiTietGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.hoVaTenLabel = new System.Windows.Forms.Label();
+            this.hoVaTenText = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.emailText = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.soDienThoaiLabel = new System.Windows.Forms.Label();
+            this.gioiTinhLabel = new System.Windows.Forms.Label();
+            this.namRadio = new System.Windows.Forms.RadioButton();
+            this.nuRadio = new System.Windows.Forms.RadioButton();
+            this.ngayLabel = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // hoVaTenLabel
+            // 
+            this.hoVaTenLabel.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.hoVaTenLabel.Location = new System.Drawing.Point(12, 98);
+            this.hoVaTenLabel.Name = "hoVaTenLabel";
+            this.hoVaTenLabel.Size = new System.Drawing.Size(100, 23);
+            this.hoVaTenLabel.TabIndex = 0;
+            this.hoVaTenLabel.Text = "Họ Và Tên";
+            // 
+            // hoVaTenText
+            // 
+            this.hoVaTenText.Location = new System.Drawing.Point(15, 135);
+            this.hoVaTenText.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.hoVaTenText.Multiline = true;
+            this.hoVaTenText.Name = "hoVaTenText";
+            this.hoVaTenText.ReadOnly = true;
+            this.hoVaTenText.Size = new System.Drawing.Size(524, 41);
+            this.hoVaTenText.TabIndex = 1;
+      
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.Location = new System.Drawing.Point(12, 202);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(44, 16);
+            this.emailLabel.TabIndex = 4;
+            this.emailLabel.Text = "Email";
+            // 
+            // emailText
+            // 
+            this.emailText.Location = new System.Drawing.Point(15, 239);
+            this.emailText.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.emailText.Multiline = true;
+            this.emailText.Name = "emailText";
+            this.emailText.ReadOnly = true;
+            this.emailText.Size = new System.Drawing.Size(524, 41);
+            this.emailText.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 353);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(524, 41);
+            this.textBox1.TabIndex = 6;
+            // 
+            // soDienThoaiLabel
+            // 
+            this.soDienThoaiLabel.Location = new System.Drawing.Point(12, 299);
+            this.soDienThoaiLabel.Name = "soDienThoaiLabel";
+            this.soDienThoaiLabel.Size = new System.Drawing.Size(116, 34);
+            this.soDienThoaiLabel.TabIndex = 7;
+            this.soDienThoaiLabel.Text = "Số Điện Thoại";
+            // 
+            // gioiTinhLabel
+            // 
+            this.gioiTinhLabel.Location = new System.Drawing.Point(12, 423);
+            this.gioiTinhLabel.Name = "gioiTinhLabel";
+            this.gioiTinhLabel.Size = new System.Drawing.Size(64, 18);
+            this.gioiTinhLabel.TabIndex = 8;
+            this.gioiTinhLabel.Text = "Giới Tính";
+            // 
+            // namRadio
+            // 
+            this.namRadio.AutoSize = true;
+            this.namRadio.Enabled = false;
+            this.namRadio.Location = new System.Drawing.Point(15, 468);
+            this.namRadio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.namRadio.Name = "namRadio";
+            this.namRadio.Size = new System.Drawing.Size(57, 20);
+            this.namRadio.TabIndex = 9;
+            this.namRadio.TabStop = true;
+            this.namRadio.Text = "Nam";
+            this.namRadio.UseVisualStyleBackColor = true;
+            // 
+            // nuRadio
+            // 
+            this.nuRadio.AutoSize = true;
+            this.nuRadio.Enabled = false;
+            this.nuRadio.Location = new System.Drawing.Point(205, 468);
+            this.nuRadio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.nuRadio.Name = "nuRadio";
+            this.nuRadio.Size = new System.Drawing.Size(45, 20);
+            this.nuRadio.TabIndex = 10;
+            this.nuRadio.TabStop = true;
+            this.nuRadio.Text = "Nữ";
+            this.nuRadio.UseVisualStyleBackColor = true;
+            // 
+            // ngayLabel
+            // 
+            this.ngayLabel.Location = new System.Drawing.Point(12, 514);
+            this.ngayLabel.Name = "ngayLabel";
+            this.ngayLabel.Size = new System.Drawing.Size(116, 27);
+            this.ngayLabel.TabIndex = 11;
+            this.ngayLabel.Text = "Ngày Sinh";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.AllowDrop = true;
+            this.dateTimePicker.Location = new System.Drawing.Point(15, 561);
+            this.dateTimePicker.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.RightToLeftLayout = true;
+            this.dateTimePicker.Size = new System.Drawing.Size(295, 22);
+            this.dateTimePicker.TabIndex = 12;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Location = new System.Drawing.Point(-1, 1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(599, 76);
+            this.panel1.TabIndex = 14;
+            this.panel1.Tag = "";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(187, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Xem Nhân Viên";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Tomato;
+            this.button1.Location = new System.Drawing.Point(192, 602);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 43);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Hủy Bỏ";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // NhanVienChiTietGUI
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 658);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.ngayLabel);
+            this.Controls.Add(this.nuRadio);
+            this.Controls.Add(this.namRadio);
+            this.Controls.Add(this.gioiTinhLabel);
+            this.Controls.Add(this.soDienThoaiLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.emailText);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.hoVaTenText);
+            this.Controls.Add(this.hoVaTenLabel);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "NhanVienChiTietGUI";
+            this.Text = "NhanVienChiTietGUIcs";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label hoVaTenLabel;
+        private System.Windows.Forms.TextBox hoVaTenText;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.TextBox emailText;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label soDienThoaiLabel;
+        private System.Windows.Forms.Label gioiTinhLabel;
+        private System.Windows.Forms.RadioButton namRadio;
+        private System.Windows.Forms.RadioButton nuRadio;
+        private System.Windows.Forms.Label ngayLabel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
